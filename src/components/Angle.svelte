bad60b255c549c94c344dde3e992abab446a6a98
Marcus Harbol <mharbol@users.noreply.github.com>
2024-06-09 15:20:57 -0600

    shameless theft

---
+++ src/components/Angle.svelte
@@ -0,0 +1,54 @@
+<script>
+    import makeArc from "@scripts/angle.ts";
+    const RADIUS = 100;
+    const HEIGHT = 200;
+    const WIDTH = 300;
+    const ARC_RAD = 20;
+    const LINE_COLOR = "#DF6247";
+    const ARC_COLOR = "#446688";
+    const ARC_WIDTH = "2";
+
+    var angle1 = 1;
+    var angle2 = 2;
+</script>
+
+<div>
+    <svg height="{HEIGHT}," width={WIDTH}>
+        <path
+            d={makeArc(
+                WIDTH / 2,
+                HEIGHT / 2,
+                ARC_RAD,
+                angle1,
+                angle2,
+                true
+            )}
+            fill="none"
+            strokeWidth="2"
+            stroke={ARC_COLOR}
+        ></path>
+        <line
+            x1={WIDTH / 2}
+            y1={HEIGHT / 2}
+            x2={RADIUS * Math.cos(angle1) + WIDTH / 2}
+            y2={RADIUS * Math.sin(angle1) + HEIGHT / 2}
+            strokeWidth={ARC_WIDTH}
+            stroke={LINE_COLOR}
+        />
+        <line
+            x1={WIDTH / 2}
+            y1={HEIGHT / 2}
+            x2={RADIUS * Math.cos(angle2) + WIDTH / 2}
+            y2={RADIUS * Math.sin(angle2) + HEIGHT / 2}
+            strokeWidth={ARC_WIDTH}
+            stroke={LINE_COLOR}
+        />
+        <circle
+            cx={WIDTH / 2}
+            cy={HEIGHT / 2}
+            r={3}
+            fill="black"
+            stroke="white"
+        />
+    </svg>
+</div>
---
+++ src/scripts/angle.ts
@@ -0,0 +1,25 @@
+
+export function daysSince(): number {
+    var now: any = new Date();
+    return Math.floor(now / 8.64e7);
+}
+
+function polarToCart(centerX: number, centerY: number, radius: number, angleRad: number): { x: number, y: number } {
+    return {
+        x: centerX + radius * Math.cos(angleRad),
+        y: centerY + radius * Math.sin(angleRad)
+    };
+}
+
+export default function makeArc(x: number, y: number, radius: number, angle1: number, angle2: number, largeArc: boolean): string {
+    const startAngle: number = angle1 < angle2 ? angle1 : angle2;
+    const endAngle: number = angle1 < angle2 ? angle2 : angle1;
+
+    var start: { x: number, y: number } = polarToCart(x, y, radius, endAngle);
+    var end: { x: number, y: number } = polarToCart(x, y, radius, startAngle);
+
+    return [
+        "M", start.x, start.y,
+        "A", radius, radius, 0, (largeArc ? "1" : "0"), (angle1 > angle2 ? "0" : "1"), end.x, end.y
+    ].join(" ");
+}
