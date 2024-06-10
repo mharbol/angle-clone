d1283533d17988d3812e4c91c9228adb2a241995
Marcus Harbol <mharbol@users.noreply.github.com>
2024-06-09 21:37:31 -0600

    svelte static

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,15 @@
 import { vitePreprocess } from '@astrojs/svelte';
+import adapter from '@sveltejs/adapter-static';
 
 export default {
 	preprocess: vitePreprocess(),
-}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assests: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        })
+    }
+};
