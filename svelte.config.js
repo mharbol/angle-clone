53e900d799f12ed8ad3c7d8114f660b9cd35a725
Marcus Harbol <mharbol@users.noreply.github.com>
2024-06-10 20:48:19 -0600

    Revert "svelte static"
    This reverts commit d1283533d17988d3812e4c91c9228adb2a241995.

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,5 @@
 import { vitePreprocess } from '@astrojs/svelte';
-import adapter from '@sveltejs/adapter-static';
 
 export default {
 	preprocess: vitePreprocess(),
-    kit: {
-        adapter: adapter({
-            pages: 'build',
-            assests: 'build',
-            fallback: undefined,
-            precompress: false,
-            strict: true
-        })
-    }
-};
+}
